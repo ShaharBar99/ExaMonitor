40d9fdcac4b27bdadd67ae730add137d5f2f4a8b
avigdorFeldman <Avigdor.Feldman@e.braude.ac.il>
2026-01-18 20:22:19 +0200

    Fixes

--- server/src/routes/incidentRoutes.js
+++ server/src/routes/incidentRoutes.js
@@ -9,6 +9,6 @@ router.use(requireAuth);
 router.post('/', IncidentController.report);
 router.get('/', IncidentController.list);
 router.post('/call-manager', IncidentController.callManager);
-router.get('/', IncidentController.listByExam); //tk added
+router.get('/:id', IncidentController.listByExam); //tk added
 
 export default router;
